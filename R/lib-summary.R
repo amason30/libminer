e696114ea5361066418a4da62e706230b40b7bb9
amason30 <masonar@umich.edu>
2023-09-18 15:01:44 -0500

    add sizes argument to lib_summary, document, and test

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -3,16 +3,36 @@
 #' Provide a brief summary of the package libraries on
 #' your machine
 #'
+#' @param size A logical value to indicate whether to calcluate library sizes. Default `FALSE`
+#'
 #' @return A `data.frame` containing the count of packages in each
-#' of the user's libraries.
+#' of the user's libraries. If `size = TRUE`, a column of library sizes will be added.
 #' @export
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+#' lib_summary(size = TRUE)
+lib_summary <- function(size = FALSE) {
+  # check sizes is logical
+  if (!is.logical(size)) {
+#    stop("sizes must be logical value (TRUE/FALSE)")
+    cli::cli_abort("size must be logical value {TRUE/FALSE}")
+  }
+
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
+
+  if (isTRUE(size)) {
+    pkg_df$lib_size <- vapply(
+      pkg_df$Library,
+      function(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      },
+      FUN.VALUE = numeric(1) # specify that the value returned should have 1 numeric value
+    )
+  }
+
   pkg_df
 }
--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -15,6 +15,16 @@ test_that("lib_summary returns expected results", {
 
 })
 
-test_that("lib_summary fails with char input", {
-  expect_error(lib_summary("foo"))
+test_that("lib_summary fails appropriately", {
+  expect_error(lib_summary("foo"), "size must be logical") # the brackets makes R think its something else and messes with error message. Not sure how this integrates with cli_abort() - maybe if I put in {} then it will be ok. A - yes! this works and you can remove ', fixed=TRUE' after the message to match
+})
+
+test_that("sizes argument works", {
+  res <- lib_summary(size = TRUE)
+  # check there are only 3 columns in the object
+  expect_equal(ncol(res), 3)
+  # check the columns names are as expected
+  expect_equal(names(res), c("Library", "n_packages", "lib_size"))
+  # check the atomic type of the returned object (logical, double, integer, etc.)
+  expect_type(res$lib_size, "double")
 })
